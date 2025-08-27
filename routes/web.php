00764febe07782eac007607cd54385a9e819766e
Moh Noor Al Azam <noor.azam@narotama.ac.id>
2025-08-27 12:20:04 +0700

    perbaiki routes untuk reset password

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,13 @@ use Illuminate\Support\Facades\Auth;
 //Login routes
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('loginform');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
+Route::get('/reset', [App\Http\Controllers\Auth\ResetController::class, 'showResetForm'])->name('resetform');
+Route::post('/reset', [App\Http\Controllers\Auth\ResetController::class, 'reset'])->name('reset');
+Route::get('/reset/{token}', [App\Http\Controllers\Auth\ResetController::class, 'newpassword'])->name('newpasswordForm');
+Route::post('/newpassword', [App\Http\Controllers\Auth\ResetController::class, 'newpassword'])->name('newpassword');
 
-Route::get('/', [App\Http\Controllers\LandingPage::class, 'index']);
+Route::get('/', [App\Http\Controllers\LandingPage::class, 'index'])->name('home');
 Route::get('/{uri}', [App\Http\Controllers\LandingPage::class, 'index']);
+
 Auth::routes();
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
